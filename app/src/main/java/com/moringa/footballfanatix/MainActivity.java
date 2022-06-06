61cfb4dc2853bedc50029ace92e6ac35d0cd52e6
kniveskatana <jellyjo5481@gmail.com>
2022-06-07 00:45:06 +0300

    adds a logic to the main activity

---
+++ app/src/main/java/com/moringa/footballfanatix/MainActivity.java
@@ -0,0 +1,32 @@
+package com.moringa.footballfanatix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnDiscover)
+    Button btnDiscover;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        btnDiscover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
